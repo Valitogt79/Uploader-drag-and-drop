ff9b242c21858493ca2103f310cc99b672c4b44c
Oswaldo Alonzo <oalonzo@gmail.com>
2021-06-25 20:05:01 -0600

    Agregando archivos para Uploader Drag and Drop

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const d = document,
+  $main = d.querySelector("main"),
+  $files = d.getElementById("files");
+
+//* Funcion para la carga de los archivos.
+const uploader = (file) => {
+  const xhr = new XMLHttpRequest(),
+    formData = new FormData();
+
+  formData.append("file", file);
+
+  //* Creando la petición
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) return;
+
+    if (xhr.status >= 200 && xhr.status < 300) {
+      let json = JSON.parse(xhr.responseText);
+      console.log(json);
+    } else {
+      let message = xhr.statusText || "Ocurrió un Error";
+      console.error(`${xhr.status}: ${message}`);
+    }
+  });
+  xhr.open("POST", "assets/uploader.php");
+  xhr.setRequestHeader("enc-type", "multipart/form-data");
+  xhr.send(formData);
+};
+
+//* Barra de progreso a la carga de los archivos.
+const progressUpload = (file) => {
+  const $progress = d.createElement("progress"),
+    $span = d.createElement("span");
+
+  $progress.value = 0;
+  $progress.max = 100;
+  $main.insertAdjacentElement("beforeend", $progress);
+  $main.insertAdjacentElement("beforeend", $span);
+
+  const fileReader = new FileReader();
+  fileReader.readAsDataURL(file);
+
+  fileReader.addEventListener("progress", (e) => {
+    //console.log(e);
+    let progress = parseInt((e.loaded * 100) / e.total);
+    $progress.value = progress;
+    $span.innerHTML = `<b>${file.name} - ${progress}%</b>`;
+  });
+  fileReader.addEventListener("loadend", (e) => {});
+  uploader(file);
+  setTimeout(() => {
+    $main.removeChild($progress);
+    $main.removeChild($span);
+    $files.value = "";
+  }, 3000);
+};
+
+d.addEventListener("change", (e) => {
+  if (e.target === $files) {
+    //console.log(e.target.files);
+
+    const files = Array.from(e.target.files);
+    files.forEach((el) => progressUpload(el));
+  }
+});
